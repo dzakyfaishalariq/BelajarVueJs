61d6887b3e8b829e20bd74bc3c35a0ca71c19cdc
dzakyfaishalariq <dzakyfaishalariq@gmail.com>
2023-05-08 13:18:28 +0700

    penggunaan API

--- latihan1/js/app.js
+++ latihan1/js/app.js
@@ -1,49 +1,16 @@
-var identitas = {
-  maximum: 25,
-  produc: [
-    {
-      id: 1,
-      gambar: "https://placeimg.com/640/480/any",
-      nama: "Kaos",
-      keterangan: "Kaos ini sangat bagus",
-      harga: 100000,
-      jumlah: 10,
-    },
-    {
-      id: 2,
-      gambar: "https://placeimg.com/640/480/any",
-      nama: "Celana",
-      keterangan: "Celana ini sangat bagus",
-      harga: 200000,
-      jumlah: 20,
-    },
-    {
-      id: 3,
-      gambar: "https://placeimg.com/640/480/any",
-      nama: "Jaket",
-      keterangan: "Jaket ini sangat bagus",
-      harga: 300000,
-      jumlah: 30,
-    },
-    {
-      id: 4,
-      gambar: "https://placeimg.com/640/480/any",
-      nama: "Topi",
-      keterangan: "Topi ini sangat bagus",
-      harga: 400000,
-      jumlah: 40,
-    },
-    {
-      id: 5,
-      gambar: "https://placeimg.com/640/480/any",
-      nama: "Sepatu",
-      keterangan: "Sepatu ini sangat bagus",
-      harga: 500000,
-      jumlah: 50,
-    },
-  ],
-};
+// penggunaan API untuk membuat aplikasi Vue.js
 var app = new Vue({
   el: "#app",
-  data: identitas,
+  data: {
+    maximum: 50,
+    products: null,
+  },
+  mounted: function () {
+    // menggunakan fetch API
+    fetch("https://hplussport.com/api/products/order/price")
+      .then((response) => response.json())
+      .then((data) => {
+        this.products = data;
+      });
+  },
 });
